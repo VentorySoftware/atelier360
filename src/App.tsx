aa704c1185fe39712dab0649fb0afb0bb752baf6
Ventory <softwareventory@gmail.com>
2025-08-24 18:52:40 -0300

    correccion archivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-la oimport { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "next-themes";
